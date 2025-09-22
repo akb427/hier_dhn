1c966744c46f388f9b23a64b067f102620fa11b2
Audrey Blizard <akb427@gmail.com>
2025-09-22 11:52:07 -0400

    Add description and comments

--- functions/partition_edges.m
+++ functions/partition_edges.m
@@ -4,26 +4,26 @@ function [pts] = partition_edges(G,w,e,n,flag)
 %
 %   [pts] = PARTITION_EDGES(G,w,e,n,flag)
 %
-%   DESCRIPTION:
-%   
+%   DESCRIPTION: Uses the Fielder vector to partition the graph G into two
+%   subgraphs based on the weights w. Because the pipes are the edges, this
+%   function also adds in the removed edges to one of the subgraphs
+%   empirically based on the existing node grouping and the missing edge
+%   type.
 %
 %   INPUTS:
-%       in1  - Description of input 1 (type, format, units if applicable)
-%       in2  - Description of input 2
+%       G       - Graph to be partitionined.
+%       w       - Vector of edge weights for partitioning.
+%       e       - Structure of edge information.
+%       n       - Structure of sizes.
+%       flag    - Binary indicating plotting of partition.
 %
 %   OUTPUTS:
-%       out1 - Description of output 1 (what it represents)
-%       out2 - Description of output 2
-%
-%   DEPENDENCIES:
-%
-%   SEE ALSO:
+%       pts - Cell of the two node groups
 
 %% Convert to Weighted Undirected Graph
 Grm = rmedge(G,e.u_idx);
 w(e.u_idx) = [];
 Gu = graph(Grm.Edges);
-
 
 %% Calculate Partition
 A = adjacency(Gu,w);
@@ -46,28 +46,40 @@ end
 G1 = subgraph(Gu,pts_idx{1});
 G2 = subgraph(Gu,pts_idx{2});
 e_inc = [G1.Edges.Names; G2.Edges.Names];
+% identify missing edges
 e_miss = setdiff(Grm.Edges.Names, e_inc);
 [e_miss_idx,~] = find(e_miss'==G.Edges.Names);
-for i = 1:numel(e_miss)
-    n_in = G.Edges.EndNodes(e_miss_idx(i),1);
-    n_out = G.Edges.EndNodes(e_miss_idx(i),2);
-    if any(e_miss(i)==e.hot)
+% for all missing edges
+for idx_e = 1:numel(e_miss)
+    % identify start and end nodes
+    n_in = G.Edges.EndNodes(e_miss_idx(idx_e),1);
+    n_out = G.Edges.EndNodes(e_miss_idx(idx_e),2);
+    % for the hot edges
+    if any(e_miss(idx_e)==e.hot)
+        % if the out node is in group one
         if any(n_out==pts_idx{1})
+            % add the in node to group one
             pts_idx{1}=[pts_idx{1} n_in];
         else
+            % add the in node to group two
             pts_idx{2}=[pts_idx{2} n_in];
         end
     else
+        % if the in node is in group one
         if any(n_in==pts_idx{1})
+            % add the out node to group one
             pts_idx{1}=[pts_idx{1} n_out];
         else
+            % add the out node to group two
             pts_idx{2}=[pts_idx{2} n_out];
         end
     end
 end
+% remove duplicates
 pts_idx = cellfun(@unique,pts_idx,'UniformOutput',false);
-for i = 1:2
-    pts{i} = G.Nodes.Names(pts_idx{i})';
+% replace index with node name
+for idx_pts = 1:2
+    pts{idx_pts} = G.Nodes.Names(pts_idx{idx_pts})';
 end
 
 
